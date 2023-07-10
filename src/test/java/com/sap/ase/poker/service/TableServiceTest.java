f94b5c159e263c0b6e8b13868cfc1169c7dad15b
Chendil Kumar V <chendil.kumar.v@sap.com>
2023-07-10 16:07:48 +0530

    Changes to add player-Chendil

--- src/test/java/com/sap/ase/poker/service/TableServiceTest.java
+++ src/test/java/com/sap/ase/poker/service/TableServiceTest.java
@@ -1,12 +1,14 @@
 package com.sap.ase.poker.service;
 
 import com.sap.ase.poker.model.GameState;
+import com.sap.ase.poker.model.Player;
 import com.sap.ase.poker.model.deck.Deck;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -32,4 +34,20 @@ class TableServiceTest {
     Assertions.assertThat(tableService.getState()).isInstanceOf(GameState.class) ;
   }
   // TODO: implement me
+
+  @Test
+  void getAllPlayers(){
+Assertions.assertThat(tableService.getPlayers()).isInstanceOf(List.class);
+  }
+
+  @Test
+  void noPlayers(){
+    Assertions.assertThat(tableService.getPlayers().size()).isEqualTo(0);
+  }
+
+  @Test
+  void addPlayer(){
+    tableService.addPlayer("01", "Chendil");
+    Assertions.assertThat(tableService.getPlayers().get(0).getName()).isEqualTo("Chendil");
+  }
 }
